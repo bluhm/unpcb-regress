020005d44db1abb8d5b46e232d0e5f8a56c7a83a
Alexander Bluhm <alexander.bluhm@gmx.net>
2015-03-04 17:46:22 +0100

    Main forks client and server.

---
+++ main.c
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2015 Alexander Bluhm <bluhm@openbsd.org>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include <err.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int	 nserver = 1;
+int	 nclient = 2;
+
+struct child {
+	pid_t	 c_pid;
+	void	(*c_func)(void);
+} *children;
+
+void	 server(void);
+void	 client(void);
+
+int
+main(int argc, char *argv[])
+{
+	struct child *c;
+	int i;
+
+	children = calloc(nserver + nclient + 1, sizeof(struct child));
+	if (children == NULL)
+		err(1, "calloc");
+	for (i = 0, c = children; i < nserver; i++, c++) {
+		c->c_func = server;
+	}
+	for (i = 0; i < nclient; i++, c++) {
+		c->c_func = client;
+	}
+
+	for (c = children; c->c_func; c++) {
+		c->c_pid = fork();
+		switch (c->c_pid) {
+		case -1:
+			err(1, "fork");
+		case 0:
+			c->c_func();
+			_exit(0);
+
+		}
+	}
+
+	return (0);
+}
+
+void
+server(void)
+{
+}
+
+void
+client(void)
+{
+}
